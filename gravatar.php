53b6d66898c45a9e6674097b2db79303c0b18b1f
Panayiotis Georgiou <panayiotisgeorgiou87@gmail.com>
2015-09-17 17:58:09 +0100

    Create gravatar.php
    The following PHP snippets can be used to determine if Gravatar is available for specified email. It returns TRUE for valid gravatar otherwise it returns false.

---
+++ gravatar.php
@@ -0,0 +1,11 @@
+function validateGravatar($email) {
+	$hash = md5(strtolower(trim($email)));
+	$uri = 'http://www.gravatar.com/avatar/' . $hash . '?d=404';
+	$headers = @get_headers($uri);
+	if (!preg_match("|200|", $headers[0])) {
+		$has_valid_avatar = FALSE;
+	} else {
+		$has_valid_avatar = TRUE;
+	}
+	return $has_valid_avatar;
+}
